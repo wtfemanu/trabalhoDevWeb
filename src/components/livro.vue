caef82ba2404a5221d4ed157fa3bad4201368a4a
wtfemanu <os9777190@gmail.com>
2025-04-24 11:16:21 -0300

    FEAT#2 tem q criar uma hq base e mudar os parametros dps

---
+++ src/components/livro.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+      <p>tem q criar um componente base dps usando as coisas ensinadas pelo eduardo vc chama e muda as proriedades</p>
+
+
+</template>
+<style></style>
