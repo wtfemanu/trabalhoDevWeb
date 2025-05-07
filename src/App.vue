7a95cecc8b47f45e9229639054b79fe90ee9aca1
wtfemanu <os9777190@gmail.com>
2025-05-07 15:08:17 -0300

    FEAT#3:fiz a lista com os livros mas ainda falta o css, boa sorte maia kkkkkkkkkkkkkkkkkkkkkkkkk

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 import SearchBar from './components/SearchBar.vue';
 import bestSell from './components/bestSell.vue';
 import HQs from './components/HQs.vue';
-
-
 </script>
+
 <template>
+
   <header>
+
     <SearchBar/>
+
   </header>
+
   <bestSell/>
+
   <HQs/>
 
 
